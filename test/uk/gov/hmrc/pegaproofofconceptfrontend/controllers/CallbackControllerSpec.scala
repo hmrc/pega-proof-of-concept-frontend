7c354a921908f4c034fe458efb1dd09f5258dbf1
Jack Gilman <56401347+DreamcastKnight@users.noreply.github.com>
2024-02-14 13:01:32 +0000

    [JG][OPS-11784] Create callback endpoint  (#4)
    * [JG] OPS-11784 added callback
    
    * [JG] OPS-11784 amended readme file
    
    * [JG] OPS-11784 addressed comments

---
+++ test/uk/gov/hmrc/pegaproofofconceptfrontend/controllers/CallbackControllerSpec.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.pegaproofofconceptfrontend.controllers
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+import play.api.http.Status
+import play.api.inject.bind
+import play.api.inject.guice.GuiceableModule
+import play.api.libs.json.JsNull
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{contentAsString, defaultAwaitTimeout, redirectLocation, status}
+import uk.gov.hmrc.auth.core.AuthConnector
+import uk.gov.hmrc.auth.core.authorise.Predicate
+import uk.gov.hmrc.auth.core.retrieve.Retrieval
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.http.test.ExternalWireMockSupport
+import uk.gov.hmrc.pegaproofofconceptfrontend.testsupport.FakeApplicationProvider
+import uk.gov.hmrc.pegaproofofconceptfrontend.utils.Generators
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class CallbackControllerSpec extends AnyWordSpec with Matchers with GuiceOneAppPerSuite with ExternalWireMockSupport with FakeApplicationProvider
+  with Generators with ScalaCheckDrivenPropertyChecks {
+
+  private val fakeAuthConnector = new AuthConnector {
+    override def authorise[A](predicate: Predicate, retrieval: Retrieval[A])(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[A] = {
+      Future.successful(JsNull.as[A](retrieval.reads))
+    }
+  }
+
+  override val overrideModules: Seq[GuiceableModule] = Seq(bind[AuthConnector].toInstance(fakeAuthConnector))
+
+  private val controller = app.injector.instanceOf[CallbackController]
+
+  private val fakeRequest = FakeRequest()
+
+  "callback" should {
+    "redirect to returns" in {
+      val result = controller.callback("")(fakeRequest)
+
+      status(result) shouldBe Status.SEE_OTHER
+      redirectLocation(result) shouldBe Some("/pega-proof-of-concept/return")
+    }
+  }
+
+  "returns" should {
+    "open to the returns page" in {
+      val result = controller.returns()(fakeRequest)
+      val doc: Document = Jsoup.parse(contentAsString(result))
+
+      status(result) shouldBe Status.OK
+      doc.select(".govuk-heading-xl").text() shouldBe "MDTP return page"
+    }
+  }
+}
